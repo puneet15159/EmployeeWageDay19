c18101eb62f117af244dfbe1808e51fb49129cbf
puneet <puneet15159@iiitd.ac.in>
2020-06-11 12:45:18 +0530

    Wage for 20 days added

--- EmpWage.java
+++ EmpWage.java
@@ -5,26 +5,31 @@ public class EmpWage {
       int salary=0;
       int empRatePerHr=20;
       int empHrs=8;
-      int a = (int)(Math.random()*((2-0)+1))+0;
-     
-    //   System.out.println(a);
-      
-      switch (a) {
+      int numberOfWorkingDays=20;
+      int totalSalary=0;
+    
+      for (int i=0;i<numberOfWorkingDays;i++){
+          int a = (int)(Math.random()*((2-0)+1))+0;
+           switch (a) {
         case 0:
             empHrs=8;
-            System.out.println("Full Time");
+            // System.out.println("Full Time");
             break;
         case 1:
             empHrs=4;
-            System.out.println("Part Time");
+            // System.out.println("Part Time");
             break;
         case 2:
             empHrs=0;
-            System.out.println("Null Time");
+            // System.out.println("Null Time");
             break;
   
-}
+    }
       salary=empHrs*empRatePerHr;
-      System.out.println("salary is: "+salary);
+      totalSalary=totalSalary+salary;
+     
+      }
+       System.out.println("Total salary for 20 days is: "+totalSalary);
+     
     }
 }
