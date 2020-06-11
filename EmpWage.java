b87eb15751320264f2c3f1d3af9b6783a3e45b6b
puneet <puneet15159@iiitd.ac.in>
2020-06-11 12:01:31 +0530

    Added daily wage calculation

--- EmpWage.java
+++ EmpWage.java
@@ -1,14 +1,20 @@
-
 import java.util.*;
 public class EmpWage {
     public static void main(String args[]) {
         
+      int salary=0;
+      int empRatePerHr=20;
+      int empHrs=8;
       double a = Math.random();
       if (a<0.5){
           System.out.println("Emplyee is present");
+          salary=empHrs*empRatePerHr;
+          System.out.println("salary is: "+salary);
       }
       else{
           System.out.println("Emplyee is absent");
+          salary=0;
+          System.out.println("salary is: "+salary);
       }
       
     }
